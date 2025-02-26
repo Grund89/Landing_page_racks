99e7907a6aab1a9f1b40c5bcb214a21b4ce310bc
Douglas Grund <douglasgrund@yahoo.com.br>
2025-02-26 00:39:04 -0300

    feat: Adiciona controller e action para contato

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+    # Renderiza a página home.html.erb
+  end
+
+  def contato
+    # Aqui você pode processar o formulário
+    nome = params[:nome]
+    telefone = params[:telefone]
+    mensagem = params[:mensagem]
+
+    # Exemplo: apenas exibe no console por agora
+    puts "Novo contato: #{nome}, #{telefone}, #{mensagem}"
+
+    # Redireciona com mensagem de sucesso
+    flash[:notice] = "Obrigada pelo contato! Entrarei em contato em breve."
+    redirect_to root_path
+  end
+end
